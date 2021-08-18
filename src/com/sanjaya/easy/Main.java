286dc63f3ee6b1142f1c8ab8908dc3ce949b050a
sanjaya kumar panigrahy <sanjaya.panigrahy@gmail.com>
2021-08-18 19:36:50 +0530

    Add files via upload
    Start Commit

---
+++ src/com/sanjaya/easy/Main.java
@@ -0,0 +1,9 @@
+package com.sanjaya.easy;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Hello Himalaya");
+    }
+}
